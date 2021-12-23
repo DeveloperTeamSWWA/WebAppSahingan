f1c7c77da3c11100a9c47c3b1b3d220f94c53e30
Mark Jude Pisan <markjude.pisan@g.batstate-u.edu.ph>
2021-12-24 02:31:40 +0800

    isSMTP change to isMail

--- WebApplicationSahingan/forgotbackend.php
+++ WebApplicationSahingan/forgotbackend.php
@@ -16,7 +16,7 @@ if(isset($_POST['send'])){
         require_once "PHPMailer/Exception.php";
         
          $mail = new PHPMailer;
-         $mail->isSMTP();                                      // Set mailer to use SMTP
+         $mail->isMail();                                      // Set mailer to use SMTP
          $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
          $mail->SMTPAuth = true;                               // Enable SMTP authentication
          $mail->Username = 'developerteamswwa@gmail.com';                 // SMTP username
