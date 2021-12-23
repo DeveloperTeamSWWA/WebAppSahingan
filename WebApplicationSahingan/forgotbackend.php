0e464c5c814051b34a6a1e869b7e70267c02c042
Mark Jude Pisan <markjude.pisan@g.batstate-u.edu.ph>
2021-12-24 02:47:42 +0800

    send mail configure

--- WebApplicationSahingan/forgotbackend.php
+++ WebApplicationSahingan/forgotbackend.php
@@ -18,11 +18,11 @@ if(isset($_POST['send'])){
          $mail = new PHPMailer;
          $mail->isSMTP();                                      // Set mailer to use SMTP
          $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-         $mail->SMTPAuth = true;                               // Enable SMTP authentication
+         $mail->SMTPAuth = false;                               // Enable SMTP authentication
          $mail->Username = 'developerteamswwa@gmail.com';                 // SMTP username
          $mail->Password = 'SWWAssociation';                           // SMTP password
          $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-         $mail->Port = 587;                                    // TCP port to connect to
+         $mail->Port = 25;                                    // TCP port to connect to
          
          $mail->setFrom('developerteamswwa@gmail.com', 'Sahingan Water Works Association');
          $mail->addAddress($email);     // Add a recipient
